ba58e05d549d196d972bd46ec502baaa1ed98675
KJY1020 <seawater1020@gmail.com>
2021-12-21 10:02:02 +0900

    파이썬 수행평가 - 가상머신 제작 second commit

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-from flask import Flask
+import sys
+from urllib import request
+from bs4 import BeautifulSoup
+ 
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return "<h1> Hello World!</h1>"
-
-@app.route('/about')
+ 
+#1
+# @app.route('/')
+# def hello_world():
+#     return '<h1> Hello from Flask!! </h1>'
+ 
+@app.route("/")
+def home():
+    return render_template('index.html', subject="안녕하세요. 반갑습니다. OOO입니다.")
+#1-1
+@app.route('/<user>')
+def hello(user):
+    return '<h1> hello ' + user
+ 
+#2
+@app.route("/about")
 def about():
-    return "about World!"
-
-if __name__ == '__main__':
-    app.run()
+    return render_template('busan1.html', subject="부산중위연령시각화")
+ 
+#3
+@app.route("/show1")
+def show1():
+    return render_template('img_test1.html', image_file='img/1.jpg') 
